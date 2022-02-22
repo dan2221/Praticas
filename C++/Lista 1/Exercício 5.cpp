e640f035cd59f94dad6f092ef5e5db2f0b9d2ef5
Daniel de Lima <36929636+dan2221@users.noreply.github.com>
2022-02-22 10:55:46 -0300

    Exercícios de C++
    Exercícios de C++ feitos durante a primeira aula de Estruturas de dados da Professora Luciana

---
+++ C++/Lista 1/Exercício 5.cpp
@@ -0,0 +1,34 @@
+// Exercício 5
+
+//Apresentar todos os valores numéricos 
+//inteiros ímpares situados na faixa de 0 a 20
+
+//Bibliotecas
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Caractéres especiais (acentos, símbolos, etc.)
+	setlocale(LC_ALL, "");
+	
+	// Variáveis da função
+	float resto;
+	int contador;
+		
+	// Limpa a tela
+	system("CLS");
+	
+	// Loop para contar os números
+	for (contador = 0; contador <=20; contador++)
+	{
+		resto = contador % 2;
+		if (resto !=0)
+		
+		{
+			cout << contador << " é impar! \n";
+		}
+	}
+	return 0; //finalizar programa
+}
