68db170754fac555d91c2f446c195ba9e371fb97
Daniel de Lima <36929636+dan2221@users.noreply.github.com>
2023-01-03 21:29:35 -0300

    Adicionados arquivos do 4º semestre

---
+++ C#/Disciplina Linguagem de Programação II/Tarefa 01 - ILPII/Tarefa 01 - ILPII/Aula04-Cadastro-veiculos/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace Aula04_Cadastro_veiculos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblCodCategoria = new System.Windows.Forms.Label();
+            this.btgravar = new System.Windows.Forms.Button();
+            this.btcancelar = new System.Windows.Forms.Button();
+            this.cboCodCategoria = new System.Windows.Forms.ComboBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtQtde = new System.Windows.Forms.TextBox();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboSupplier = new System.Windows.Forms.ComboBox();
+            this.lblSupplier = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtUnitInStock = new System.Windows.Forms.TextBox();
+            this.txtUnitInOrder = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtReorder = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(129, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(130, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Quantidade:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(130, 213);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Preço unitário:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(130, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Categoria:";
+            // 
+            // lblCodCategoria
+            // 
+            this.lblCodCategoria.AutoSize = true;
+            this.lblCodCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodCategoria.Location = new System.Drawing.Point(425, 132);
+            this.lblCodCategoria.Name = "lblCodCategoria";
+            this.lblCodCategoria.Size = new System.Drawing.Size(19, 15);
+            this.lblCodCategoria.TabIndex = 6;
+            this.lblCodCategoria.Text = "...";
+            // 
+            // btgravar
+            // 
+            this.btgravar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btgravar.Location = new System.Drawing.Point(159, 431);
+            this.btgravar.Name = "btgravar";
+            this.btgravar.Size = new System.Drawing.Size(89, 23);
+            this.btgravar.TabIndex = 7;
+            this.btgravar.Text = "Gravar";
+            this.btgravar.UseVisualStyleBackColor = true;
+            this.btgravar.Click += new System.EventHandler(this.Btgravar_Click);
+            // 
+            // btcancelar
+            // 
+            this.btcancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btcancelar.Location = new System.Drawing.Point(332, 431);
+            this.btcancelar.Name = "btcancelar";
+            this.btcancelar.Size = new System.Drawing.Size(89, 23);
+            this.btcancelar.TabIndex = 8;
+            this.btcancelar.Text = "Cancelar";
+            this.btcancelar.UseVisualStyleBackColor = true;
+            // 
+            // cboCodCategoria
+            // 
+            this.cboCodCategoria.FormattingEnabled = true;
+            this.cboCodCategoria.Location = new System.Drawing.Point(278, 129);
+            this.cboCodCategoria.Name = "cboCodCategoria";
+            this.cboCodCategoria.Size = new System.Drawing.Size(121, 21);
+            this.cboCodCategoria.TabIndex = 9;
+            this.cboCodCategoria.SelectedIndexChanged += new System.EventHandler(this.Cbocodcategoria_SelectedIndexChanged);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(278, 47);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(174, 20);
+            this.txtNome.TabIndex = 10;
+            // 
+            // txtQtde
+            // 
+            this.txtQtde.Location = new System.Drawing.Point(278, 170);
+            this.txtQtde.Name = "txtQtde";
+            this.txtQtde.Size = new System.Drawing.Size(174, 20);
+            this.txtQtde.TabIndex = 12;
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Location = new System.Drawing.Point(278, 210);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(174, 20);
+            this.txtUnitPrice.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(129, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID Fornecedor:";
+            this.label1.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // cboSupplier
+            // 
+            this.cboSupplier.FormattingEnabled = true;
+            this.cboSupplier.Location = new System.Drawing.Point(278, 88);
+            this.cboSupplier.Name = "cboSupplier";
+            this.cboSupplier.Size = new System.Drawing.Size(121, 21);
+            this.cboSupplier.TabIndex = 9;
+            this.cboSupplier.SelectedIndexChanged += new System.EventHandler(this.Cbocodcategoria_SelectedIndexChanged);
+            // 
+            // lblSupplier
+            // 
+            this.lblSupplier.AutoSize = true;
+            this.lblSupplier.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSupplier.Location = new System.Drawing.Point(425, 89);
+            this.lblSupplier.Name = "lblSupplier";
+            this.lblSupplier.Size = new System.Drawing.Size(19, 15);
+            this.lblSupplier.TabIndex = 6;
+            this.lblSupplier.Text = "...";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(130, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 15);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Uni. em estoque:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(130, 293);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(109, 15);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Uni. em pedido:";
+            // 
+            // txtUnitInStock
+            // 
+            this.txtUnitInStock.Location = new System.Drawing.Point(278, 250);
+            this.txtUnitInStock.Name = "txtUnitInStock";
+            this.txtUnitInStock.Size = new System.Drawing.Size(174, 20);
+            this.txtUnitInStock.TabIndex = 12;
+            // 
+            // txtUnitInOrder
+            // 
+            this.txtUnitInOrder.Location = new System.Drawing.Point(278, 290);
+            this.txtUnitInOrder.Name = "txtUnitInOrder";
+            this.txtUnitInOrder.Size = new System.Drawing.Size(174, 20);
+            this.txtUnitInOrder.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(130, 331);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(101, 15);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Reorder Level:";
+            // 
+            // txtReorder
+            // 
+            this.txtReorder.Location = new System.Drawing.Point(278, 328);
+            this.txtReorder.Name = "txtReorder";
+            this.txtReorder.Size = new System.Drawing.Size(174, 20);
+            this.txtReorder.TabIndex = 13;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(129, 373);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(107, 15);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "Descontinuado:";
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(277, 370);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(174, 20);
+            this.txtDesc.TabIndex = 13;
+            this.txtDesc.TextChanged += new System.EventHandler(this.TextBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 494);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.txtReorder);
+            this.Controls.Add(this.txtUnitInOrder);
+            this.Controls.Add(this.txtUnitInStock);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.txtQtde);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.cboSupplier);
+            this.Controls.Add(this.cboCodCategoria);
+            this.Controls.Add(this.btcancelar);
+            this.Controls.Add(this.btgravar);
+            this.Controls.Add(this.lblSupplier);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lblCodCategoria);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblCodCategoria;
+        private System.Windows.Forms.Button btgravar;
+        private System.Windows.Forms.Button btcancelar;
+        private System.Windows.Forms.ComboBox cboCodCategoria;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtQtde;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboSupplier;
+        private System.Windows.Forms.Label lblSupplier;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtUnitInStock;
+        private System.Windows.Forms.TextBox txtUnitInOrder;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtReorder;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtDesc;
+    }
+}
+
---
+++ C#/Disciplina Linguagem de Programação II/Tarefa 01 - ILPII/Tarefa 01 - ILPII/Aula04-Cadastro-veiculos/Form1.cs
@@ -0,0 +1,120 @@
+﻿// Tarefa de ILP2
+
+//Com base no formulário de Cadastro de Veículos
+//Crie um Formulário de cadastro para a tabela de PRODUCTS  do banco de dados NORTHWIND.
+//Utilizando ComboBox para as colunas que são chaves estrangeiras SupplierID e CategoryID.
+
+//sendo que:
+
+//Para a ComboBox de SupplierID,
+//o DisplayMember (o que vai ser exibido) será a coluna CompanyName;
+//o DisplayValue (o valor que vai ser utilizado) será a própria coluna  SupplierID;
+
+
+//Para a ComboBox de CategoryID, 
+//o DisplayMember (o que vai ser exibido) será a coluna CategoryName;
+//o DisplayValue (o valor que vai ser utilizado) será a própria coluna  CategoryID;
+
+//OBS.: ENTREGAR UM ARQUIVO DE TEXTO DO WORD COM O PRINT DA TELA E O CÓDIGO 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+// Bibliotecas SQL //////////
+using System.Data;
+using System.Data.SqlClient;
+/////////////////////////////
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula04_Cadastro_veiculos
+{
+    public partial class Form1 : Form
+    {
+        // Declaração de atributos da classe pública ////////////////////////////////////
+        // Eles usam tipos de dados obtidos das bibliotecas SQL.
+
+        // O objeto do tipo SqlConnection têm a função de gerar uma conexão
+        // com um banco de dados sendo, portanto o objeto fundamental no acesso a dados.
+        private SqlConnection conexao;
+
+        // O SqlDataAdapter, serve como uma ponte entre um DataSet e o SQL Server para
+        // recuperar e salvar dados.
+        private SqlDataAdapter adapter;
+
+        private SqlCommand comando;
+
+        // O DataTable funciona como um conjunto de linhas e colunas na memória que
+        // armazenam os dados filtrados e descarregados pelo DataAdapter, e pode ser
+        // considerado a principal classe do ADO.NET para armazenar dados desconectados.
+        private DataTable tblProducts;
+        private string strsql, strconex;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Se estiver usando o SQL Server Express Edition altere a parte "source=(local)" para "source=\\LAB6-14".
+            strconex = "data source=(local);initial catalog=locadora;integrated security=sspi";
+            conexao = new SqlConnection(strconex);
+            conexao.Open();
+
+            // Instanciando objeto do tipo DataTable.
+            tblProducts = new DataTable();
+
+            strsql = "SELECT * FROM Products";
+            // O Objeto do tipo SqlDataAdapter serve para retornar o que eu quero da tabela.
+            // Pode ser feito em forma de select para filtrar o resultado.
+            adapter = new SqlDataAdapter(strsql, conexao);
+
+            // O método Fill da Classe SqlDataAdapter serve para PREENCHER A TABELA do banco de dados ativo.
+            adapter.Fill(tblProducts);
+
+            // DataSource, DisplayMember e ValueMember são atributos do Objeto ComboBox.
+            cboCodCategoria.DataSource = tblProducts; // Fonte de dados, neste caso é o objeto tblProducts.
+            cboCodCategoria.DisplayMember = "CategoryName"; // O que vai ser exibido
+            cboCodCategoria.ValueMember = "CategoryID";
+
+            // Exibe um código da categoria dentro da label
+            lblCodCategoria.Text = cboCodCategoria.SelectedValue.ToString();
+
+            // Repetição dos procedimentos para a ComboBox Supplier(Fornecedor)
+            cboSupplier.DataSource = tblProducts;
+            cboSupplier.DisplayMember = "CompanyName";
+            cboSupplier.ValueMember = "SupplierID";
+
+            lblSupplier.Text = cboSupplier.SelectedValue.ToString();
+        }
+
+        private void Cbocodcategoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Atualiza a label do código da categoria
+            lblCodCategoria.Text = cboCodCategoria.SelectedValue.ToString();
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btgravar_Click(object sender, EventArgs e)
+        {
+            strsql = "INSERT INTO veiculos (ProductName, SupplierID, CategoryID, QuantityPerUnit, UnitPrice, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued) " +
+                "VALUES ('" + txtNome.Text + "','" + cboSupplier.SelectedValue + "','" + cboCodCategoria.SelectedValue + "','" + txtQtde.Text + "','" + txtUnitInStock.Text + "','" + txtUnitInOrder.Text + "','" + txtReorder.Text + "','" + txtDesc.Text + "')";
+            comando = new SqlCommand(strsql, conexao);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Gravado com sucesso!", "mensagem");
+        }
+    }
+}
